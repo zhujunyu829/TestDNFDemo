2ae11287648e76c392351e68f1cc9591711ab9f9
zhujunyu <zhujunyu@172-1-5-250.lightspeed.hstntx.sbcglobal.net>
2017-04-12 15:58:32 +0800

    新建项目

---
+++ DNFDemo/DNFDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DNFDemo
+//
+//  Created by ZhuJunyu on 2017/4/12.
+//  Copyright © 2017年 ZhuJunyu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ DNFDemo/DNFDemoUITests/DNFDemoUITests.m
@@ -0,0 +1,40 @@
+//
+//  DNFDemoUITests.m
+//  DNFDemoUITests
+//
+//  Created by ZhuJunyu on 2017/4/12.
+//  Copyright © 2017年 ZhuJunyu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DNFDemoUITests : XCTestCase
+
+@end
+
+@implementation DNFDemoUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
